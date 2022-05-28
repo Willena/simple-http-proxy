f6399ea4937f35dbce5d6b8d204c6916bd7e826e
Guillaume Villena <guillaume@villena.me>
2022-05-28 12:37:09 +0200

    Suport for SSL targets

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ docker run \
   -d \
   --name simple-proxy \
   -e UPSTREAM=example.com \
+  -e SSL=false \
   -p 80:80 \
 pure/simple-reverse-proxy
 ```
@@ -18,3 +19,5 @@ example.com
 some.server.com:8080
 10.10.10.10:8000
 ```
+
+Value for `SSL` is true or false. If the target is using SSL then the `SSL` variable should be `true`
--- init.sh
+++ init.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-envsubst '$UPSTREAM' </usr/local/etc/haproxy/haproxy.cfg.tmpl >/usr/local/etc/haproxy/haproxy.cfg
+export SSL_LINE=""
+
+if [[ $SSL == "true" ]]
+then
+  export SSL_LINE="ssl verify none"
+fi
+
+
+envsubst </usr/local/etc/haproxy/haproxy.cfg.tmpl >/usr/local/etc/haproxy/haproxy.cfg
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg
